88a7daf42e0268395b084dec55d1b6e41595b813
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-14 11:49:29 +0100

    added controllers, routes and user model.

--- back-end/server.js
+++ back-end/server.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const blogRoutes = require('./routes/blog');
 
 const app = express();
 
@@ -18,11 +19,15 @@ if (process.env.NODE_ENV === 'development') {
   app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
 
-app.get('/api', (req, res) => {
-  res.json({ time: Date().toString() });
-});
+app.use('/api', blogRoutes);
 
 const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL)
+  .then(() => console.log('db connected succesfully'))
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  });
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
-After set-up the app, please add your .env file to main directory which contains all env variables.
+Notes for back-end
+
+After download source codea and set-up the app, please add your .env file to main directory which contains all env variables. Then run
+`npm start`
+in order to start server.
+
+
