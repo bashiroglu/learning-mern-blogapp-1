a15ee15d422505128644d4fdfc887b95be73fb14
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-14 18:18:39 +0100

    front-end, corrected documentjs and added config

--- front-end/pages/_document.js
+++ front-end/pages/_document.js
@@ -12,7 +12,9 @@ class MyDocument extends Document {
           />
           <link
             rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+            crossOrigin="anonymous"
           />
         </Head>
         <body>
--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,12 +1,11 @@
-
 import Layout from '../components/Layout';
 
 const Index = () => {
-    return (
-        <Layout>
-            <h2>Index page</h2>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <h2>Index page</h2>
+    </Layout>
+  );
 };
 
 export default Index;
