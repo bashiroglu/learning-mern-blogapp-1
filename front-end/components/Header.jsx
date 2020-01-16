0891c22459f77c6586d776316a5dac48b946ac77
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-16 12:43:34 +0100

    added Dashboard item to Header

--- front-end/components/Header.jsx
+++ front-end/components/Header.jsx
@@ -43,6 +43,26 @@ const Header = () => {
               </React.Fragment>
             )}
 
+            {isAuth() && isAuth().role === 0 && (
+              <NavItem>
+                <Link href="/user">
+                  <NavLink style={{ cursor: 'pointer' }}>{`${
+                    isAuth().name
+                  }'s Dashboard`}</NavLink>
+                </Link>
+              </NavItem>
+            )}
+
+            {isAuth() && isAuth().role === 1 && (
+              <NavItem>
+                <Link href="/admin">
+                  <NavLink style={{ cursor: 'pointer' }}>{`${
+                    isAuth().name
+                  }'s Dashboard`}</NavLink>
+                </Link>
+              </NavItem>
+            )}
+
             {isAuth() && (
               <NavItem>
                 <NavLink
